c101e70646adcce487382be3dcc847745a57911b
Christian Schwede <christian.schwede@enovance.com>
2014-11-14 13:58:29 +0000

    Make the adminuser independent from LDAP and Github
    SF needs an admin user during deployment to create the config repository (for
    example). However, currently this is only possible with a configured LDAP
    backend or the built-in dummy LDAP, that is only available on domain tests.dom.
    
    This patches modifies the authentication and allows to configure another
    admin user that is independent from all other authentication sources.
    
    The admin password is stored hashed and salted on the managesf node.
    
    Change-Id: I8c71b2f84f201cae3959017916d2f54180499aed

---
+++ tools/cauth/cauth/adminsettings.py
@@ -0,0 +1,10 @@
+# THIS FILE IS ONLY AN EXAMPLE, CONTENTS WILL BE OVERWRITTEN BY PUPPET
+#
+# You need to use a hashed password; use the following code to create one:
+#
+# mkpasswd -m sha-512 "secret_password"
+
+username = "user1"
+password = "REPLACE_THIS_WITH_A_SALTED_HASH"
+mail = "user1@example.com"
+lastname = "Admin user"
