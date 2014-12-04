10e904dce58ce2683f2653e8a1ece02ca7e8494e
Fabien Boucher <fabien.boucher@enovance.com>
2014-12-04 17:34:15 +0000

    WIP - manage zuul and jjb base files during the upgrade
    Change-Id: If6ef5c6941e264248cb7338e3e83d89a28787eff

--- tests/roles_provision/prepare.sh
+++ tests/roles_provision/prepare.sh
@@ -3,7 +3,7 @@
 . ../../role_configrc
 
 echo "Archive software factory local GIT copy"
-(cd ../../; tar -czf /tmp/software-factory-lc.tgz *)
+(cd ../../; tar -czf /tmp/software-factory-lc.tgz .)
 echo "Create install-server-vm archive for version $SF_VER to ${INST}/install-server-vm-${SF_VER}.edeploy"
 (cd ${INST}/install-server-vm; tar -c -p --use-compress-program=pigz -f ../install-server-vm-${SF_VER}.edeploy .)
 echo "Create softwarefactory archive for version $SF_VER to ${INST}/softwarefactory-${SF_VER}.edeploy"
--- upgrade.sh
+++ upgrade.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-source role_configrc
-
-# Version must be passed as argument here
 wanted=$1
+
+clone_path=/srv/software-factory
+[ ! -d "$clone_path" ] && exit 1
+
+# Check that we are able to jump on the request tag (SF version)
+cd $clone_path && git checkout $wanted || {
+    echo "$wanted version does not exist yet ! exit"
+    exit 1
+}
+
+# Within the tag check we will be able to upgrade the current running SF version
+source role_configrc
+current_version=$(edeploy version | cut -d'-' -f2)
+[ "$current_version" != "${PREVIOUS_SF_VER}" ] && {
+    echo "The current version of Software Factory running here"
+    echo "cannot be updated. From $wanted we are only able to"
+    echo "update from ${PREVIOUS_SF_VER} to ${SF_VER}"
+    exit 1
+}
 
 # get the domain from sfconfig.yaml
 DOMAIN=$(egrep "^domain:" /etc/puppet/hiera/sf/sfconfig.yaml | sed 's/domain://' | tr -d ' ')
 
-# Clone Software-Factory and jump in the right tag
-clone_path=/srv/software-factory
-if [ ! -d "$clone_path" ]; then
-    git clone http://softwarefactory.enovance.com/r/software-factory $clone_path
-    cd $clone_path
-    git checkout $wanted
-fi
-
-# We should make so verification
-# Are we able to upgrade the JJB and Zuul provided conf
-echo
-
-# Start the upgrade by jumping in the cloned version
-cd /srv/software-factory/upgrade/${PREVIOUS_SF_VER}/${SF_VER}/
+# Start the upgrade by jumping in the cloned version and running
+# the ansible playbook.
+cd ${clone_path}/upgrade/${PREVIOUS_SF_VER}/${SF_VER}/
 cp group_vars/all.tmpl group_vars/all
 sed -i "s/FROM/${PREVIOUS_SF_VER}/" group_vars/all
 sed -i "s/TO/${SF_VER}/" group_vars/all
 sed -i "s/DOMAIN/${DOMAIN}/" group_vars/all
+sed -i "s/CLONE_PATH/${clone_path}/" group_vars/all
 ansible-playbook -i hosts site.yml
+cd -
