4d9bf7add4f2c0f63a41c329e2420f384a6385ef
Christian Schwede <christian.schwede@enovance.com>
2014-12-04 08:24:19 +0000

    Remove Debian support
    Debian support is not tested since awhile, and in fact it is broken right now.
    Let's remove Debian support and clean up our edeploy und puppet files.
    
    Change-Id: I5050afda0c3ce2b34df7e145adf3c64dbde3fa2f

--- puppet/modules/commonservices-apache/manifests/init.pp
+++ puppet/modules/commonservices-apache/manifests/init.pp
@@ -14,32 +14,11 @@
 # under the License.
 
 class commonservices-apache {
-
-  case $operatingsystem {
-    centos: {
-      $http = "httpd"
-      $provider = "systemd"
-      $gateway_conf = "/etc/httpd/conf.d/gateway.conf"
-      $httpd_user = "apache"
-    }
-
-    debian: {
-      $http = "apache2"
-      $provider = "debian"
-      $gateway_conf = "/etc/apache2/sites-available/gateway"
-      $httpd_user = "www-data"
-
-      exec {'enable_gateway':
-        command => 'a2ensite gateway',
-        path    => '/usr/sbin/:/usr/bin/:/bin/',
-        require => File['gateway_conf'],
-        before  => Class['monit'],
-      }
-    }
-  }
+  $http = "httpd"
+  $httpd_user = "apache"
 
   file {'gateway_conf':
-    path   => $gateway_conf,
+    path   => '/etc/httpd/conf.d/gateway.conf',
     ensure => file,
     mode   => '0640',
     owner  => $httpd_user,
