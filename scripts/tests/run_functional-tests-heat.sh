5505f9bc6acb158d97c54cdb7326da7cd26449b5
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-12-07 17:27:14 +0000

    scripts: move and refactor most scripts
    This changes try to clean and unify most used scripts to a common
    directory.
    Symlinks have been created to provide retrocompatibility.
    
    Change-Id: I2e8979ff8b5df302fcb5f2de1517c51438418dcc

---
+++ scripts/tests/run_functional-tests-heat.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This script will build if needed the roles for software-factory
+# Then will start the SF using the HEAT deployment way
+# Then will run the serverspecs and functional tests
+
+[ -f /etc/tenant_openrc ] && {
+    echo "Found a pre-configured tenant. Use it"
+    source /etc/tenant_openrc
+}
+
+[ -z "$OS_TENANT_ID" ] && {
+    echo "OS_TENANT_ID is empty ... have you sourced an openrc ?"
+    exit 1
+}
+echo "Use $OS_TENANT_NAME on $OS_AUTH_URL"
+
+source $(dirname $(readlink $0 || echo $0))/functestslib.sh
+cd ${SF_ROOT}
+
+echo "Running functional-tests with this HEAD"
+display_head
+
+set -x
+
+uuid=$(date +%s | sha256sum | base64 | head -c 5)
+export STACKNAME=sf_${uuid}
+
+function get_ip {
+    while true; do
+        p=`nova list | grep ${STACKNAME}-puppetmaster | cut -d'|' -f7  | awk '{print $NF}' | sed "s/ //g"`
+        [ -n "$p" ] && break
+        sleep 10
+    done
+    echo $p
+}
+
+function waiting_stack_deleted {
+    while true; do
+        heat stack-list | grep -i $STACKNAME | grep -i failed
+        [ "$?" -eq "0" ] && {
+            echo "Stack deletion has failed ..."
+            return 255
+        }
+        heat stack-list | grep -i $STACKNAME
+        [ "$?" != "0" ] && break
+        sleep 60
+    done
+}
+
+function heat_stop {
+    if [ ! ${SF_SKIP_BOOTSTRAP} ]; then
+        if [ ! ${DEBUG} ]; then
+            cd bootstraps/heat
+            ./start.sh delete_stack
+            waiting_stack_deleted
+            if [ "$?" = "255" ]; then
+                # Retry a deletion call
+                ./start.sh delete_stack
+                waiting_stack_deleted
+            fi
+            cd -
+        fi
+    fi
+}
+
+function build_imgs {
+    if [ ! ${SF_SKIP_BUILDROLES} ]; then
+        VIRT=1 ./build_roles.sh &> ${ARTIFACTS_DIR}/build_roles.sh.output || pre_fail "Roles building FAILED"
+    fi
+}
+
+function heat_start {
+    if [ ! ${SF_SKIP_BOOTSTRAP} ]; then
+        cd bootstraps/heat
+        ./start.sh full_restart_stack
+        cd -
+    fi
+}
+
+function glance_delete_images {
+    cd bootstraps/heat
+    ./start.sh delete_images
+    cd -
+}
+
+set -x
+prepare_artifacts
+checkpoint "$(date) - $(hostname)"
+build_imgs
+checkpoint "build_roles"
+heat_start
+checkpoint "heat_start"
+waiting_stack_created $STACKNAME
+checkpoint "wait_heat_stack"
+run_tests 15
+checkpoint "run_tests"
+DISABLE_SETX=1
+checkpoint "end_tests"
+get_logs
+checkpoint "get-logs"
+heat_stop
+checkpoint "heat_stop"
+glance_delete_images
+checkpoint "glance_delete_images"
+publish_artifacts
+checkpoint "publish-artifacts"
+exit 0;
---
+++ scripts/tests/run_functional-tests.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This script will build if needed the roles for software-factory
+# Then will start the SF in LXC containers
+# Then will run the serverspecs and functional tests
+
+source $(dirname $(readlink $0 || echo $0))/functestslib.sh
+cd ${SF_ROOT}
+
+echo "Running functional-tests with this HEAD"
+display_head
+
+function lxc_stop {
+    if [ ! ${SF_SKIP_BOOTSTRAP} ]; then
+        if [ ! ${DEBUG} ]; then
+            (cd bootstraps/lxc; ./start.sh clean &> ${ARTIFACTS_DIR}/lxc-clean.output)
+        fi
+    fi
+}
+
+function build {
+    if [ ! ${SF_SKIP_BUILDROLES} ]; then
+        clear_mountpoint
+        ./build_roles.sh ${ARTIFACTS_DIR} || pre_fail "Roles building FAILED"
+    fi
+}
+
+function lxc_start {
+    if [ ! ${SF_SKIP_BOOTSTRAP} ]; then
+        clear_mountpoint
+        cd bootstraps/lxc
+        ./start.sh stop &> ${ARTIFACTS_DIR}/lxc-stop.output
+        ./start.sh &> ${ARTIFACTS_DIR}/lxc-start.output || pre_fail "LXC bootstrap FAILED"
+        cd -
+    fi
+}
+
+set -x
+prepare_artifacts
+checkpoint "$(date) - $(hostname)"
+(cd bootstraps/lxc; ./start.sh clean &> ${ARTIFACTS_DIR}/lxc-first-clean.output)
+checkpoint "lxc-first-clean"
+build
+checkpoint "build_roles"
+if [ -z "$1" ]; then
+    # This test is run by default when no argument provided
+    lxc_start
+    checkpoint "lxc-start"
+    run_tests 15
+    checkpoint "run_tests"
+fi
+if [ "$1" == "backup_restore_tests" ]; then
+    lxc_start
+    checkpoint "lxc-start"
+    run_backup_restore_tests 45 "provision" || pre_fail "Backup test: provision"
+    lxc_stop
+    lxc_start
+    run_backup_restore_tests 45 "check" || pre_fail "Backup test: check"
+fi
+if [ "$1" == "upgrade" ]; then
+    cloned=/tmp/software-factory # The place to clone the previous SF version to deploy
+    (
+        [ -d $cloned ] && rm -Rf $cloned
+        git clone http://softwarefactory.enovance.com/r/software-factory $cloned
+        cd $cloned
+        # Be sure to checkout the right previous version
+        git checkout $PREVIOUS_SF_REL
+        # Fetch the pre-built images
+        ./fetch_roles.sh trees
+        # Trigger a build role in order to deflate roles in the right directory if not done yet
+        ./build_roles.sh
+        cd bootstraps/lxc
+        # Deploy
+        ./start.sh
+        cd ../..
+        source functestslib.sh
+        wait_for_bootstrap_done
+        # Run basic tests
+        run_serverspec
+    ) || pre_fail "Stable Bootstrap FAILED"
+    # Run the provisioner to put some data on the deployed instance
+    ./tools/provisioner_checker/run.sh provisioner
+    # Put needed data to perform the upgrade on the deployed instance
+    # In a real upgrade process this won't be done because next version
+    # (The one we want to upgrade is already published)
+    (
+        cd tests/roles_provision/
+        sudo ./prepare.sh
+        export ANSIBLE_HOST_KEY_CHECKING=False
+        ansible-playbook --private-key=${HOME}/.ssh/id_rsa -i inventory playbook.yaml
+    ) || pre_fail "Ansible provision playbook FAILED"
+    # Start the upgrade
+    ssh -o StrictHostKeyChecking=no root@`get_ip puppetmaster` "cd /srv/software-factory/ && ./upgrade.sh ${SF_REL}" || pre_fail "Upgrade FAILED"
+    # Run basic tests
+    run_serverspec || pre_fail "Serverspec failed"
+    # Run the checker to validate provisionned data has not been lost
+    ./tools/provisioner_checker/run.sh checker || pre_fail "Provisionned data check failed"
+fi
+
+DISABLE_SETX=1
+checkpoint "end_tests"
+get_logs
+checkpoint "get-logs"
+lxc_stop
+checkpoint "lxc-stop"
+publish_artifacts
+checkpoint "publish-artifacts"
+exit 0;
