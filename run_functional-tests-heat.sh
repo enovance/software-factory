ce3237439b80bf4ba8ff46e6fcda18c767079c9b
Tristan Cacqueray <tristan.cacqueray@enovance.com>
2014-12-09 22:00:41 +0000

    functional_tests-heat: fix keypair and other fixs
    * Make sure the keypair is well registered
    * Adds a trap to terminate the stack
    * Adds "retries" to prevent infini loop
    * Fix host puppet module requirement for redmine and others modules
    * Disable SELinux
    
    Change-Id: I68acd04086546d4992ec5cafffc614d5de25dcbd

--- run_functional-tests-heat.sh
+++ run_functional-tests-heat.sh
@@ -36,16 +36,34 @@ set -x
 
 export STACKNAME=SoftwareFactory
 
+function check_keypair {
+    LOCAL_FINGERPRINT=$(ssh-keygen -l -f ~/.ssh/id_rsa | awk '{ print $2 }')
+    NOVA_KEYPAIR=$(nova keypair-list | grep ${HEAT_TENANT} | awk '{ print $4 }')
+
+    if [ -z "${NOVA_KEYPAIR}" ]; then
+        echo "Creating a new keypair for ${HEAT_TENANT}"
+        nova keypair-add --pub-key ~/.ssh/id_rsa.pub ${HEAT_TENANT}
+    elif [ "${NOVA_KEYPAIR}" != "${LOCAL_FINGERPRINT}" ]; then
+        echo "Replacing old keypair for ${HEAT_TENANT}"
+        nova keypair-delete ${HEAT_TENANT}
+        nova keypair-add --pub-key ~/.ssh/id_rsa.pub ${HEAT_TENANT}
+    fi
+}
+
 function get_ip {
+    RETRIES=0
     while true; do
         p=`nova list | grep ${STACKNAME}-puppetmaster | cut -d'|' -f7  | awk '{print $NF}' | sed "s/ //g"`
         [ -n "$p" ] && break
+        let RETRIES=RETRIES+1
+        [ "$RETRIES" == "40" ] && exit 1
         sleep 10
     done
     echo $p
 }
 
 function waiting_stack_deleted {
+    RETRIES=0
     while true; do
         heat stack-list | grep -i $STACKNAME | grep -i failed
         [ "$?" -eq "0" ] && {
@@ -54,6 +72,8 @@ function waiting_stack_deleted {
         }
         heat stack-list | grep -i $STACKNAME
         [ "$?" != "0" ] && break
+        let RETRIES=RETRIES+1
+        [ "$RETRIES" == "40" ] && exit 1
         sleep 60
     done
 }
@@ -76,7 +96,7 @@ function heat_stop {
 
 function build_imgs {
     if [ ! ${SF_SKIP_BUILDROLES} ]; then
-        VIRT=1 ./build_roles.sh &> ${ARTIFACTS_DIR}/build_roles.sh.output || pre_fail "Roles building FAILED"
+        VIRT=1 ./build_roles.sh ${ARTIFACTS_DIR} || pre_fail "Roles building FAILED"
     fi
 }
 
@@ -89,17 +109,21 @@ function heat_start {
 }
 
 function glance_delete_images {
-    cd bootstraps/heat
-    ./start.sh delete_images
-    cd -
+    if [ ! ${DEBUG} ]; then
+        (cd bootstraps/heat; ./start.sh delete_images)
+    fi
 }
+
+
 
 set -x
 prepare_artifacts
 checkpoint "$(date) - $(hostname)"
 build_imgs
 checkpoint "build_roles"
+check_keypair
 heat_start
+trap "echo 'cleaning...'; heat_stop &> /dev/null; glance_delete_images &> /dev/null;" 0 1 2 4 5 6 7 8 9 10 11 12 13 14 15 16
 checkpoint "heat_start"
 waiting_stack_created $STACKNAME
 checkpoint "wait_heat_stack"
@@ -113,6 +137,7 @@ heat_stop
 checkpoint "heat_stop"
 glance_delete_images
 checkpoint "glance_delete_images"
+trap - 0 1 2 4 5 6 7 8 9 10 11 12 13 14 15 16
 publish_artifacts
 checkpoint "publish-artifacts"
 exit 0;
