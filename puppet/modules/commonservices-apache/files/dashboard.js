4084e953f3c4263486930fa93321d49becdf05d2
George Peristerakis <george.peristerakis@enovance.com>
2014-12-18 10:48:14 -0500

    AChanges the REST API to introduce groups
    Change-Id: Icd0edff2c1812d44a484be6eb2684558256e3dff

--- puppet/modules/commonservices-apache/files/dashboard.js
+++ puppet/modules/commonservices-apache/files/dashboard.js
@@ -2,10 +2,12 @@ var sfDashboard = angular.module('sfDashboard', []);
 
 function mainController($scope, $http) {
     $scope.data = {};
+    $scope.members= [];
 
-    function get_projects() {
+    function initProjects() {
         $scope.errors = false;
         $scope.loading = true;
+
         $http.get('/manage/project/')
             .success(function(data) {
                 $scope.projects = data;
@@ -15,56 +17,67 @@ function mainController($scope, $http) {
             }).finally(function () {
                 $scope.loading = false;
             });
-    }
+    };
 
-    get_projects();
-
-    function get_open_reviews() {
+    function initReviews() {
         $http.get('/r/changes/?q=status:open')
             .success(function(data) {
-        var reviews = {};
+                var reviews = {};
                 for (key in data) {
-                     var p = data[key].project;
-                     if (reviews[p] == null) {
+                    var p = data[key].project;
+                    if (reviews[p] == null) {
                         reviews[p] = 0;
-            }
-                     reviews[p]++;
+                    }
+                    reviews[p]++;
                 }
                 $scope.open_reviews = reviews;
             })
             .error(function(data) {
                 $scope.errors = data;
             });
-    }
+    };
 
-    get_open_reviews();
-
-    function get_open_bugs() {
+    function initBugs() {
         $http.get('/redmine//issues.json?status_id=open')
             .success(function(data) {
-        var bugs = {};
-                for (key in data['issues']) {
-                     p = data['issues'][key]["project"]["name"];
-                     if (bugs[p] == null) {
+                var bugs = {};
+                for (var key in data['issues']) {
+                    var p = data['issues'][key]["project"]["name"];
+                    if (bugs[p] == null) {
                         bugs[p] = {};
                         bugs[p]['id'] = data['issues'][key]["project"]["id"];
                         bugs[p]['count'] = 0;
-            }
-                     bugs[p]['count']++;
+                    }
+                    bugs[p]['count']++;
                 }
                 $scope.open_bugs = bugs;
-
             })
             .error(function(data) {
                 $scope.errors = data;
             });
-    }
+    };
 
-    get_open_bugs();
+    function initMembers() {
+        $http.get('/manage/members/')
+            .success( function (data) {
+                $scope.members(data);
+            })
+            .error( function (data) {
+                $scope.errors = data;
+            });
+    };
+
+    function init() {
+        initProjects();
+        initReviews();
+        initBugs();
+        initMembers();
+    };
 
     $scope.createProject = function() {
         $scope.errors = false;
         $scope.loading = true;
+
         $http.put('/manage/project/' + $scope.data.name , $scope.data)
             .success(function(data) {
                 $scope.data = {};
@@ -80,6 +93,7 @@ function mainController($scope, $http) {
     $scope.deleteProject = function(name) {
         $scope.errors = false;
         $scope.loading = true;
+
         $http.delete('/manage/project/' + name)
             .success(function(data) {
                 $scope.data = {};
@@ -91,4 +105,68 @@ function mainController($scope, $http) {
                 $scope.loading = false;
             });
     };
+
+    $scope.selectMembers = function(selectedGroup) {
+
+        if ( $scope.selectedMembers.length < 1 ) {
+            return;
+        }
+
+        $scope.errors = false;
+        $scope.loading = true;
+
+        var addMemberToGroup = function (item, index, members) {
+            var url = '/manage/project/membership/' + $scope.selectedProject + '/' + item.id + '/';
+
+            $http.put(url, {groups: [selectedGroup]} )
+                .success( function(data) {
+                    selectedGroup.members.push(item);
+                })
+                .error( function(data) {
+                    selectedGroup.members.push(item);
+                    $scope.errors = data;
+                });
+        };
+
+        $scope.selectedMembers.map(addMemberToGroup);
+        $scope.loading = false;
+	$scope.selectedMembers = [];
+    };
+
+    $scope.deselectMembers = function(selectedGroup) {
+
+	if (selectedGroup.members.length < 1){
+	    return;
+	}
+
+        $scope.errors = false;
+        $scope.loading = true;
+
+        var removeMemberFromGroup = function (item, index, members) {
+
+            var url = '/manage/project/membership/' + $scope.selectedProject
+                    + '/' + item.id + '/' + selectedGroup.name;
+
+            selectedGroup.members.splice(index, 1);
+
+            $http.delete(url)
+                .success( function(data) {
+                    selectedGroup.members.splice(index, 1);
+                    console.log(data);
+                })
+                .error( function(data) {
+                    $scope.errors(data);
+                });
+        };
+
+        selectedGroup.members.map(removeMemberFromGroup);
+        $scope.loading = false;
+	$scope.selectedMembers = [];
+    };
+
+    $scope.membershipForm = function(project) {
+        $scope.selectedProject = project;
+    };
+
+    init();
 }
