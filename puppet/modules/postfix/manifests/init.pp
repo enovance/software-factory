a99b2f25d6ea988bf5c20595c04d5ed002cf7755
Christian Schwede <christian.schwede@enovance.com>
2015-01-05 14:26:40 +0000

    Ensure postfix user and group exists
    Change-Id: Id72aa4fc48d5520988ed6cab42f556d916de7516

--- puppet/modules/postfix/manifests/init.pp
+++ puppet/modules/postfix/manifests/init.pp
@@ -18,8 +18,19 @@ class postfix ($settings = hiera_hash('postfix', '')) {
 
   $provider = "systemd"
 
+  group { 'postfix':
+    ensure => present,
+  }
+
+  user { 'postfix':
+    ensure  => present,
+    gid     => 'postfix',
+    require => Group['postfix'],
+  }
+
   package { 'postfix':
     ensure => 'installed',
+    require => [User['postfix'], Group['postfix']],
   }
 
   exec { '/etc/mailname':
