f3d10951e03a5eb3043fc1df1f8e19ec2e725f9c
Fabien Boucher <fabien.boucher@enovance.com>
2015-01-13 17:52:13 +0000

    Open TCP flow for jenkins swarm slave
    - Enable a socat TCP redirect from managesf (Gateway)
      to jenkins for TCP/8080 and TCP/45452
    - For HEAT open those port on the security group
    - For LXC add two iptables rule to redirect those
      port from the Host to managesf
    
    Change-Id: I15c6a9c83096ce9b13e390ec9118d938bcbc7540

--- bootstraps/lxc/start.sh
+++ bootstraps/lxc/start.sh
@@ -56,9 +56,10 @@ function setup_iptables {
     sudo iptables -t nat $switch PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to-destination 192.168.134.54:80
     # Redirect host incoming TCP/29418 to the sf gateway on 192.168.134.54/29418 (a socat service listens 29418 to redirect internally to the Gerrit service)
     sudo iptables -t nat $switch PREROUTING -p tcp -i eth0 --dport 29418 -j DNAT --to-destination 192.168.134.54:29418
-    # Redirect host incoming TCP/8080 to the sf gateway on 192.168.134.54/8080 (a socat service listens 8080 to redirect internally to the Jenkins service)
-    # TODO: this socat service need to be enabled
+    # Redirect host incoming TCP/8080 and TCP/45452 to the sf gateway on 192.168.134.54 (a socat service listens 8080 and 45452 to redirect internally to the Jenkins service)
+    # This is to allow jenkins swarm (slave) to SF via the gateway
     sudo iptables -t nat $switch PREROUTING -p tcp -i eth0 --dport 8080 -j DNAT --to-destination 192.168.134.54:8080
+    sudo iptables -t nat $switch PREROUTING -p tcp -i eth0 --dport 45452 -j DNAT --to-destination 192.168.134.54:45452
     set -e
 }
 
