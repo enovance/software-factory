aa4c4c219c49dbd0988500efd9b78d7221f05452
Matthieu Huin <mhu@enovance.com>
2015-03-30 14:36:26 +0000

    get members of a given group in gerrit with pysflib
    Change-Id: I1cf466aeca53765faa797bbd6be6e96720e4e57f

--- tools/pysflib/pysflib/sfgerrit.py
+++ tools/pysflib/pysflib/sfgerrit.py
@@ -234,21 +234,27 @@ class GerritUtils:
         except HTTPError as e:
             return self._manage_errors(e)
 
-    def get_group_member_id(self, group_id, username=None, mail=None):
+    def get_group_members(self, group_id):
         try:
-            resp = self.g.get('groups/%s/members/' % group_id)
-            if username:
-                uid = [m['_account_id'] for m in resp if
-                       m['username'] == username]
-            if mail:
-                uid = [m['_account_id'] for m in resp if
-                       m['email'] == mail]
-            if uid:
-                return uid[0]
-            else:
-                return None
+            return self.g.get('groups/%s/members/' % group_id)
         except HTTPError as e:
             return self._manage_errors(e)
+
+    def get_group_member_id(self, group_id, username=None, mail=None):
+        resp = self.get_group_members(group_id)
+        if not resp:
+            # hitting the HTTPError, returning the result of _manage_errors
+            return resp
+        uid = [None, ]
+        if username and mail:
+            raise ValueError("Filter by username or mail, not both")
+        if username:
+            uid = [m['_account_id'] for m in resp if
+                   m['username'] == username]
+        elif mail:
+            uid = [m['_account_id'] for m in resp if
+                   m['email'] == mail]
+        return uid[0]
 
     def get_group_owner(self, name):
         try:
