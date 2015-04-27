6b573655dbd50d3cc48762bd7d532870356a0e20
Christian Schwede <christian.schwede@enovance.com>
2015-04-27 08:44:55 +0000

    Bring back the version to the page title
    The title string is no longer shown because the topmenu iframe is no longer the
    parent of the window. This change appends the SF version tag to all titles;
    unfortunately Gerrit overwrites this later, thus it is shown everywhere except
    in Gerrit.
    
    Change-Id: I8f0acc04c83167cfc63b99356c17435076f50a1d

--- puppet/modules/commonservices-apache/files/topmenu.js
+++ puppet/modules/commonservices-apache/files/topmenu.js
@@ -5,6 +5,7 @@ function loadTopmenu() {
         iframe.setAttribute("height", "51px");
         iframe.frameBorder = 0;
         document.body.insertBefore(iframe, document.body.firstChild);
+        document.title += ' [SF 0.9.8]';
 };
 
 if (document.body) {
