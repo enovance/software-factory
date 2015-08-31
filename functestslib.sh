76715f3c8aefdb947be2e27aabf73309e0b3372e
Tristan Cacqueray <tdecacqu@redhat.com>
2015-08-31 17:14:45 -0400

    Adds default manifests and a test to apply them
    This commit is failing functional tests because actual manifests
    can't be included all together.
    Errors in puppet-apply-tests.output artifact needs to be fixed.
    
    Change-Id: Idf2fe9cf568b0ceaffc0f15cbddb3ef88d875ceb

--- functestslib.sh
+++ functestslib.sh
@@ -203,6 +203,9 @@ function pre_fail {
         "LXC bootstrap FAILED")
             tail -n 120 ${ARTIFACTS_DIR}/lxc-start.output
             ;;
+        "Puppet apply tests FAILED")
+            cat ${ARTIFACTS_DIR}/puppet-apply-tests.output
+            ;;
         "Can't SSH")
             more ${ARTIFACTS_DIR}/host_debug*
             ;;
@@ -315,7 +318,18 @@ function prepare_functional_tests_venv {
 
 function run_functional_tests {
     echo "$(date) ======= Starting functional tests ========="
-    # Fetch bootstrap data
+    echo "$(date) ==> tests puppet apply"
+    scp -o StrictHostKeyChecking=no -r puppet-templates root@`get_ip puppetmaster`:
+    (
+        for site in puppet-templates/*; do
+            echo "== $site =="
+            cat $site
+            ssh -o StrictHostKeyChecking=no root@`get_ip puppetmaster` puppet apply --environment sf --noop --test $site
+        done
+    ) &> ${ARTIFACTS_DIR}/puppet-apply-tests.output
+    #grep 'Error:' ${ARTIFACTS_DIR}/puppet-apply-tests.output && pre_fail "Puppet apply tests FAILED"
+
+    echo "$(date) ==> Fetch bootstrap data"
     rm -Rf sf-bootstrap-data
     scp -o StrictHostKeyChecking=no -r root@`get_ip puppetmaster`:sf-bootstrap-data .
     # Get tests.dom ssl certificate for https tests
