3f127cb13e432706487fded9781c4a29014ecb9c
Tristan Cacqueray <tdecacqu@redhat.com>
2015-09-03 14:37:03 -0400

    Remove puppet agent from replication test
    Instead of using puppet agent to restart gerrit, the test can follow the
    documentation and restart gerrit manually.
    
    Change-Id: Ie3f7e93ac0b656033545e79419d5850ef82ec469

--- tests/functional/test_09_replication.py
+++ tests/functional/test_09_replication.py
@@ -166,13 +166,15 @@ class TestProjectReplication(Base):
         # Create new section for this project in replication.config
         self.createConfigSection(self.un, self.pname)
 
-        # Force gerrit to read its known_hosts file. The only
-        # way to do that is by restarting gerrit. The Puppet Gerrit
-        # manifest will restart gerrit if a new entry in known_hosts_gerrit
-        # is discovered.
-        # This may take some time (gerrit in some condition take long
-        # to be fully up)
-        call("ssh root@gerrit puppet agent -t > /dev/null", shell=True)
+        # Append ssh config configuration for replication url
+        call("ssh root@gerrit.tests.dom tee -a /home/gerrit/.ssh/config<<EOF\n"
+             "Host 'tests.dom'\n"
+             "  PreferredAuthentications publickey\n"
+             "  StrictHostKeyChecking no\n"
+             "  UserKnownHostsFile /dev/null\n"
+             "EOF\n", shell=True)
+        # Restart gerrit service
+        call("ssh root@gerrit.tests.dom systemctl restart gerrit", shell=True)
 
         # Clone the project and submit it for review
         priv_key_path = set_private_key(config.USERS[self.un]["privkey"])
