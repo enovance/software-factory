06060c28df8d32fd213bd492e5226bc53aeee5ab
Fabien Boucher <fabien.boucher@enovance.com>
2015-09-16 20:33:25 +0000

    Rework sf_slave_setup.sh no longer rely on sfstack
    Change-Id: I01e0d848952bd0569078198ad435baa101c8fa1b

--- puppet/modules/nodepool/files/sf_slave_setup.sh
+++ puppet/modules/nodepool/files/sf_slave_setup.sh
@@ -2,19 +2,30 @@
 
 . base.sh
 
-# Prepare slave for SF
+# This should be removed after we fix functestlib.sh and how
+# we manage to store artifacts
+sudo useradd www-data
+
+sudo yum install -y epel-release
+
+sudo yum install -y git python-augeas bridge-utils curl lxc wget swig python-devel python-pip graphviz python-yaml
+sudo pip install flake8 bash8
+sudo pip install -U tox==1.6.1 virtualenv==1.10.1 Sphinx oslosphinx
+
+sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
+sudo chmod 600 /srv/swap
+sudo mkswap /srv/swap
+grep swap /etc/fstab || echo "/srv/swap none swap sw 0 0" | sudo tee -a /etc/fstab
+
 sudo sed -i 's/^.*SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
-git clone http://softwarefactory.enovance.com/r/sfstack
-cd sfstack && sudo ./sfinstall.sh
-sudo mkdir /srv/deps
-sudo chown -R jenkins /srv/
-cd /srv/software-factory && ./fetch_roles.sh bases
-cd /srv/software-factory && ./fetch_roles.sh trees
-sudo chmod 777 /var/lib/sf/
-ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa
-sudo chown -R jenkins /home/jenkins/.ssh
+
+sudo git clone https://github.com/enovance/edeploy-lxc.git /srv/edeploy-lxc
+
+sudo mkdir -p /var/lib/sf
+sudo mkdir -p /var/lib/sf/artifacts/logs
+sudo chown -R jenkins:jenkins /var/lib/sf/
 
 # sync FS, otherwise there are 0-byte sized files from the yum/pip installations
 sudo sync
 
-echo "setup.sh finished. Creating snapshot now, this will take a few minutes"
+echo "Setup finished. Creating snapshot now, this will take a few minutes"
