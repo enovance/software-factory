80de504f2a484036daff1e2ef13d25d85e636d15
Tristan Cacqueray <tdecacqu@redhat.com>
2015-09-29 16:52:08 -0400

    Clean image building/publishing process
    * Move cache to /sf/roles/cache
    * Rename edeploy into image
    * Rename .md5 into .hash
    * Both cache and image now produce:
    ** .edeploy, a tarball
    ** .hash, git hash used to produces cache
    ** .pip, pip freeze output
    ** .rpm, rpm -qa output
    ** .md5sum, a sum of all the above
    
    Change-Id: Ia07acbab4329f1c93a0ecf59af5d648d1f749ea0

---
+++ publish_image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# publish code borrowed from publish_docs script
+
+set -e
+[ -n "$DEBUG" ] && set -x
+
+. ./role_configrc
+
+CONTAINER="edeploy-roles"
+
+function publish {
+    SRC=$1
+    IMG=$(basename $SRC)
+    IMG_NAME=$2
+    cd $(dirname $SRC)
+    echo "[+] Check if $IMG have changed"
+    UPSTREAM_HASH=$(curl -s ${SWIFT_SF_URL}/${IMG_NAME}.hash &> /dev/null)
+    # Swift does not return 404 but 'Not Found'
+    [ "$(cat ${IMG_NAME}.hash)" == "${UPSTREAM_HASH}" ] && return
+    echo "[+] Upstream is out dated"
+    if [ ! -f "${IMG_NAME}.edeploy" ]; then
+        echo "[+] Creating edeploy file of ${SRC}"
+        (cd $IMG; sudo tar -c -p --use-compress-program=pigz -f ../${IMG_NAME}.edeploy .)
+    fi
+    echo "[+] Creating manifest"
+    OBJ="$(/bin/ls ${IMG_NAME}.{edeploy,hash,pip,rpm,img.qcow2} 2> /dev/null || true)"
+    md5sum $OBJ | sudo tee ${IMG_NAME}.md5sum
+    for OBJECT in $OBJ ${IMG_NAME}.md5sum; do
+        [ -f ${OBJECT} ] || continue
+        echo "[+] Uploading ${OBJECT} to ${SWIFT_BASE_URL}/v1/AUTH_${SWIFT_ACCOUNT}/${CONTAINER}/"
+        SWIFT_PATH="/v1/AUTH_${SWIFT_ACCOUNT}/${CONTAINER}/${OBJECT}"
+        set +x
+        TEMPURL=`swift tempurl PUT 120 ${SWIFT_PATH} ${TEMP_URL_KEY}`
+        set -x
+        curl -f -i -X PUT --upload-file "$OBJECT" "${SWIFT_BASE_URL}${TEMPURL}"
+    done
+}
+
+echo "=== Publish image ${IMAGE_PATH} ==="
+publish ${IMAGE_PATH} softwarefactory-${SF_VER}
+echo "=== Publish cache ${CACHE_PATH} ==="
+publish ${CACHE_PATH} sf-centos7
