e0e4e3f7548b58aa58350226dfa8607b5b310e08
Fabien Boucher <fabien.boucher@enovance.com>
2015-10-19 13:01:36 +0000

    Remove public_ip setting from nodepool sfconfig.yaml
    This setting is not needed today and was confusing. The
    original idea was to give a way to slaves to contact SF.
    
    Regarding the slave agent of Jenkins deployed on node, this
    setting is not needed as this is the Jenkins master that connect
    to the slave to initiate the slave agent.
    
    Change-Id: I6f13a9c76831064aa1cd4d3781196eb5a847c5b3

--- config/scripts/validate_sfconfig.py
+++ config/scripts/validate_sfconfig.py
@@ -11,6 +11,9 @@ except:
 
 # Adds default values and validation here
 
+# Remove public_ip setting of nodepool (2.0.1 -> 2.0.2)
+if 'public_ip' in d['nodepool']:
+    del d['nodepool']['public_ip']
 
 yaml.dump(d, open(argv[1], "w"), default_flow_style=False)
 exit(0)
