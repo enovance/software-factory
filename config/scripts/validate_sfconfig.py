947583014a54f722d416b4e7183a52f2b7b87265
Fabien Boucher <fabien.boucher@enovance.com>
2015-10-19 13:52:52 +0000

    Enable disabled for swift artifact export in sfconfig.yaml
    Change-Id: I731459f15cfa06b75166b39417d0b249238bdf31

--- config/scripts/validate_sfconfig.py
+++ config/scripts/validate_sfconfig.py
@@ -11,6 +11,9 @@ except:
 
 # Adds default values and validation here
 
+# Use disabled key instead (2.0.1 -> 2.0.2)
+if 'swift_logsexport_activated' in d['logs']:
+    del d['logs']['swift_logsexport_activated']
 
 yaml.dump(d, open(argv[1], "w"), default_flow_style=False)
 exit(0)
