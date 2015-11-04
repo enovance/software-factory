df40df4775ce893744ce5077d8748911e423962a
Christian Schwede <cschwede@redhat.com>
2015-11-04 12:59:33 +0000

    Add Graphite, Grafana and Statsd
    The used version of graphite must be run using the top url ('/'), thus another
    port on the Apache server is used for this. Grafana is listening on port 3000.
    
    Change-Id: I8365f19c1a67e2b466c0107e7d0bb60088f45fa9

---
+++ puppet/modules/graphite/files/local_settings.py
@@ -0,0 +1,25 @@
+GRAPHITE_ROOT = '/usr/share/graphite'
+
+CONF_DIR = '/etc/graphite-web'
+STORAGE_DIR = '/var/lib/graphite-web'
+CONTENT_DIR = '/usr/share/graphite/webapp/content'
+
+DASHBOARD_CONF = '/etc/graphite-web/dashboard.conf'
+GRAPHTEMPLATES_CONF = '/etc/graphite-web/graphTemplates.conf'
+
+WHISPER_DIR = '/var/lib/carbon/whisper'
+RRD_DIR = '/var/lib/carbon/rrd'
+DATA_DIRS = [WHISPER_DIR, RRD_DIR] # Default: set from the above variables
+LOG_DIR = '/var/log/graphite-web/'
+INDEX_FILE = '/var/lib/graphite-web/index'  # Search index file
+
+DATABASES = {
+    'default': {
+        'NAME': '/opt/graphite/storage/graphite.db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
