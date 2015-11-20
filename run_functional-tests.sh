29de317dcda30e2a6755c025dcf2e9f9974274e6
Tristan Cacqueray <tdecacqu@redhat.com>
2015-11-20 15:45:55 -0500

    OpenStack based test support
    This change adds required batteries to build an OpenStack based
    continous integration:
    * A nodepool image builder to prepare a centos_rdo_setup.sh
    * A jjb job to deploy and configure a nested rdo cloud
    
    More over this change adds a new type of functional test to be used
    to validate OpenStack integration. This tests will deploy SF using Heat
    and run functional tests.
    
    Change-Id: I111f8298fdaf0f05e709e28b4dcea70333e4ab21

--- run_functional-tests.sh
+++ run_functional-tests.sh
@@ -32,6 +32,9 @@ display_head
 prepare_artifacts
 checkpoint "Running tests on $(hostname)"
 lxc_stop
+if [ ${TEST_TYPE} == "openstack" ] && [ ! -f "${IMAGE_PATH}.img.qcow3" ]; then
+    export BUILD_QCOW=1
+fi
 build_image
 
 case "${TEST_TYPE}" in
@@ -62,6 +65,43 @@ case "${TEST_TYPE}" in
         run_checker
         run_serverspec_tests
         run_functional_tests
+        ;;
+    "openstack")
+        glance image-create --progress --disk-format qcow2 --container-format bare --name sf-${SF_VER} --file ${IMAGE_PATH}-${SF_VER}.img.qcow2
+        NET_ID=$(neutron net-list | grep 'external_network' | awk '{ print $2 }' | head)
+        GLANCE_ID=$(glance image-list | grep "sf-${SF_VER}" | awk '{ print $2 }' | head)
+        heat stack-create --template-file ./deploy/heat/softwarefactory.hot -P "sf_root_size=10;key_name=id_rsa;domain=tests.dom;image_id=${GLANCE_ID};ext_net_uuid=${NET_ID};flavor=m1.medium" sf_stack
+        RETRY=40
+        while [ $RETRY -gt 0 ]; do
+            STACK_STATUS=$(heat stack-show sf_stack | grep 'stack_status ' | awk '{ print $4 }')
+            [ "${STACK_STATUS}" != "CREATE_IN_PROGRESS" ] && break
+            sleep 1
+            let RETRY--
+        done
+        if [ "${STACK_STATUS}" == "CREATE_COMPLETE" ]; then
+            IP=$(heat stack-show sf_stack | grep "Public address of the SF instance" | sed 's/.*: //' | awk '{ print $1 }' | sed 's/..$//')
+            PASSWORD=$(heat stack-show sf_stack | grep "Administrator password for SF services" | sed 's/.*: //' | awk '{ print $1 }' | sed 's/..$//')
+            ROUTER=$(neutron router-list | grep 'sf_stack' | awk '{ print $2 }')
+            if [ ! -n "${IP}" ] || [ ! -n "${PASSWORD}" ] || [ ! -n "${ROUTER}" ]; then
+                echo "Couldn't retrieve stack paramters..."
+                exit 1
+            fi
+            NETNS="sudo ip netns exec qrouter-${ROUTER}"
+            sleep 5;
+            $NETNS ping -c 1 -w 1 ${IP} || exit -1
+            RETRY=40
+            while [ $RETRY -gt 0 ]; do
+                $NETNS curl 192.168.200.13 | grep dashboad && break
+                sleep 1
+                let RETRY--
+            done
+            $NETNS ssh -i ${HOME}/.ssh/id_rsa ssh root@${IP} "ps afx; edeploy version"
+        else
+            heat stack-show sf_stack
+            heat resource-list sf_stack
+            exit 1
+        fi
+        exit 0
         ;;
     *)
         echo "[+] Unknown test type ${TEST_TYPE}"
