285a4ed9eba121ea378594442b0ba763f40435a6
Matthieu Huin <mhu@enovance.com>
2015-12-10 15:02:05 +0100

    Add a user for automated tasks
    We define a new non-admin user that will be used for generic, automated
    tasks such as remote hook calls. Its credentials are generated automatically.
    It is not intended to be used for anything else.
    
    Change-Id: I9d026d61f3d8fea5ad1e2300419c22ca3bb0f07f

--- config/scripts/sfconfig.sh
+++ config/scripts/sfconfig.sh
@@ -78,6 +78,7 @@ function generate_yaml {
     GRAPHITE_MYSQL_SECRET=$(generate_random_pswd 32)
     GRAPHITE_SECRET_KEY=$(generate_random_pswd 32)
     GRAFANA_MYSQL_SECRET=$(generate_random_pswd 32)
+    SF_HOOKS_SECRET=$(generate_random_pswd 32)
     sed -i "s#MYSQL_ROOT_PWD#${MYSQL_ROOT_SECRET}#" ${OUTPUT}/sfcreds.yaml
     sed -i "s#REDMINE_SQL_PWD#${REDMINE_MYSQL_SECRET}#" ${OUTPUT}/sfcreds.yaml
     sed -i "s#GERRIT_SQL_PWD#${GERRIT_MYSQL_SECRET}#" ${OUTPUT}/sfcreds.yaml
@@ -87,6 +88,7 @@ function generate_yaml {
     sed -i "s#GRAPHITE_SQL_PWD#${GRAPHITE_MYSQL_SECRET}#" ${OUTPUT}/sfcreds.yaml
     sed -i "s#GRAPHITE_SECRET_KEY#${GRAPHITE_SECRET_KEY}#" ${OUTPUT}/sfcreds.yaml
     sed -i "s#GRAFANA_SQL_PWD#${GRAFANA_MYSQL_SECRET}#" ${OUTPUT}/sfcreds.yaml
+    sed -i "SF_HOOKS_PWD#${SF_HOOKS_SECRET}#" ${OUTPUT}/sfcreds.yaml
     # Default authorized ssh keys on each node
     JENKINS_PUB="$(cat ${BUILD}/ssh_keys/jenkins_rsa.pub | cut -d' ' -f2)"
     SERVICE_PUB="$(cat ${BUILD}/ssh_keys/service_rsa.pub | cut -d' ' -f2)"
