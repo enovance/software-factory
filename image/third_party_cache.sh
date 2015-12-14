5e0639a74ac5fa0bdcbc4826d6e81c5f93f51b86
Christian Schwede <cschwede@redhat.com>
2015-12-14 11:08:01 +0000

    Use local Swift cache for third party tools if possible
    Iterates over all defined variable names ending with a "_URL" and replaces the
    URL with the name of a local Swift container. The container must be public
    readable and should contain all the required files. A small helper function has
    been added to download all the files to the Swift container.
    
    This does not cache requirements that are downloaded during installation of
    packages, but at least caches all third party artifacts.
    
    Change-Id: I88460e34e8d1b104781a466b8efc2b6a3478e9ae

---
+++ image/third_party_cache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SRC=$(cd $(dirname $0); pwd)
+
+. ${SRC}/third_party_tools
+
+#---- Download tarballs to local Swift cache -----
+# Swift env variables need to be set for this, for example:
+# export ST_AUTH=http://127.0.0.1:8080/auth/v1.0
+# export ST_USER=test:tester
+# export ST_KEY=secret
+
+for url in $URL_LIST; do
+  filename=`echo "${url}" | sed 's/.*\///'`
+  if [[ ${filename} == *.* ]] ; then  # Only for files
+      curl -o /tmp/${filename} "${url}"
+      swift upload cache /tmp/${filename} --object-name ${filename}
+      rm /tmp/${filename}
+  fi
+done
