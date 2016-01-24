cf8bff3e45e298aabdc1e3842379cd0d67ef14f3
Tristan Cacqueray <tdecacqu@redhat.com>
2016-01-24 08:58:03 -0500

    nodepool+zuul: update version
    * Make zuul service run as zuul
    * Import systemd service files from ansible role
    * Use environment file to set statsd host
    * Add nodepool secure.conf and logging.conf, adapt yaml configuration:
      http://docs.openstack.org/infra/nodepool/configuration.html
    
    Cosmetics:
    * Remove un-used debian init script
    * Make SKIP_BUILD shows what is updated
    
    WIP:
    * Fix current zuul master reload hang:
      https://storyboard.openstack.org/#!/story/2000472
    
    Change-Id: I6493049e78ee5397618b3771c9244a2265e46cb7

--- functestslib.sh
+++ functestslib.sh
@@ -194,13 +194,13 @@ function build_image {
         echo "SKIP_BUILD: Reusing previously built image, just update source code without re-installing"
         echo "            To update requirements and do a full installation, do not use SKIP_BUILD"
         set -e
-        sudo rsync -a --delete puppet/manifests/ ${IMAGE_PATH}/etc/puppet/environments/sf/manifests/
-        sudo rsync -a --delete puppet/modules/ ${IMAGE_PATH}/etc/puppet/environments/sf/modules/
-        sudo rsync -a --delete puppet/hiera/ ${IMAGE_PATH}/etc/puppet/hiera/sf/
-        sudo rsync -a --delete config/ansible/ ${IMAGE_PATH}/usr/local/share/sf-ansible/
-        sudo rsync -a --delete config/config-repo/ ${IMAGE_PATH}/usr/local/share/sf-config-repo/
-        sudo cp -Rv config/scripts/* ${IMAGE_PATH}/usr/local/bin/
-        sudo cp -Rv config/defaults/* ${IMAGE_PATH}/etc/puppet/hiera/sf/
+        sudo rsync -aiv --delete puppet/manifests/ ${IMAGE_PATH}/etc/puppet/environments/sf/manifests/
+        sudo rsync -aiv --delete puppet/modules/ ${IMAGE_PATH}/etc/puppet/environments/sf/modules/
+        sudo rsync -aiv --delete puppet/hiera/ ${IMAGE_PATH}/etc/puppet/hiera/sf/
+        sudo rsync -aiv --delete config/ansible/ ${IMAGE_PATH}/usr/local/share/sf-ansible/
+        sudo rsync -aiv --delete config/config-repo/ ${IMAGE_PATH}/usr/local/share/sf-config-repo/
+        sudo rsync -aiv config/scripts/ ${IMAGE_PATH}/usr/local/bin/
+        sudo rsync -aiv config/defaults/ ${IMAGE_PATH}/etc/puppet/hiera/sf/
         echo "SKIP_BUILD: direct copy of ${MANAGESF_CLONED_PATH}/ to ${IMAGE_PATH}/var/www/managesf/"
         sudo rsync -a --delete ${MANAGESF_CLONED_PATH}/ ${IMAGE_PATH}/var/www/managesf/
         echo "SKIP_BUILD: direct copy of ${CAUTH_CLONED_PATH}/ to ${IMAGE_PATH}/var/www/cauth/"
