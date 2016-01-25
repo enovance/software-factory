79fa5f0bbe5df8adf3551bc832451e2a7be24192
fabien Boucher <fabien.boucher@enovance.com>
2016-01-25 12:22:41 +0100

    Move libvirt-daemon-lxc libvirt install in rpm-requirements.sh
    Furthermore remove it from deploy.py.
    
    On a fresh centos 7 using ststack.sh libvirt is missing. So
    this patch move those requirements at a most proper place.
    
    Change-Id: I0822948ea609a4460574e0faf5bf05c03095bbe5

--- rpm-requirements.sh
+++ rpm-requirements.sh
@@ -15,6 +15,7 @@ which pip &> /dev/null          || PKGS="${PKGS} python-pip"
 [ -f "/usr/include/ffi.h" ]     || PKGS="${PKGS} libffi-devel mariadb-devel openldap-devel openssl-devel"
 [ -f "/usr/include/python2.7/Python.h" ] || PKGS="${PKGS} python-devel"
 [ -d "/usr/lib64/python2.7/site-packages/Crypto/" ] || PKGS="${PKGS} python2-crypto"
+[ -f "/usr/lib64/libvirt/connection-driver/libvirt_driver_lxc.so" ] || PKGS="${PKGS} libvirt-daemon-lxc libvirt"
 
 if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
     echo "(+) Adds epel-release..."
@@ -22,6 +23,7 @@ if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
 fi
 
 if [ ! -z "${PKGS}" ]; then
-    echo "(+) Installing build requirement..."
+    echo "(+) Installing build requirements..."
     sudo yum install -y $PKGS
+    sudo systemctl restart libvirtd
 fi
