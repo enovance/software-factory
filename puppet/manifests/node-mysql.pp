5d5707d96e16dcc1e7442d75142be723bab96d35
Tristan Cacqueray <tdecacqu@redhat.com>
2016-02-06 16:57:11 -0500

    Add distributed architecture
    This new 'distributed' REFARCH defines a fully distributed
    environment to demonstrate SF configuration support such deployment.
    Only LXC is currently supported. To do so, here are the required changes:
    
    * Update sfconfig.sh and deploy.py to support multi-nodes
    * Bring back socat_gerrit to support gerrit on external node
    * Fix known_hosts using ansible and global configuration in /etc/ssh
    * Fix replication test to support remote gerrit
    
    Minor edits included:
    * "Band-aid" task to check redmine 404
    * Support ansible fact caching in /var/lib/ansible/
    * Simplify init-config script and correct default replication name
    
    Change-Id: I1c6da007b5ac77091df65817cbf1afa371272edb

---
+++ puppet/manifests/node-mysql.pp
@@ -0,0 +1,14 @@
+Package {
+  allow_virtual => false,
+}
+
+$httpd_user = 'apache'
+
+node default {
+  include ::sfbase
+  include ::bup
+  include ::postfix
+  include ::monit
+
+  include ::mysql
+}
