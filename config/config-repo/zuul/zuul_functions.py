049bd0bd2294db86591896c5c7a1ff20d1414d4e
Tristan Cacqueray <tdecacqu@redhat.com>
2016-02-18 15:03:04 -0500

    zuul: add functions.py to offline nodepool slave
    When a job has a node_label different from 'master' or
    '.*publish.*', set the OFFLINE_NODE_WHEN_COMPLETE to make sure the
    node won't get re-used in jenkins.
    
    Change-Id: I9c0c522a89fd739d2b87d275e576446c66ffeec9

---
+++ config/config-repo/zuul/zuul_functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+def set_node_options(item, job, params):
+    print "XXXDEBUG: set_node_options(", item, job, params, ")"
+    build = item.current_build_set.getBuild(job.name)
+    print "XXXDEBUG: build(", job.name, ") is", build
+    if build:
+        # check if job node is master or publish label
+        offline_node = True
+        for node_label in build.node_labels:
+            if node_label == 'master' or node_label.find('publish') != -1:
+                offline_node = False
+                break
+    else:
+        # otherwise don't offline the node
+        offline_node = False
+
+    if offline_node:
+        print "XXXDEBUG: Setting OFFLINE_NODE_WHEN_COMPLETE for job %s" % job.name
+        params['OFFLINE_NODE_WHEN_COMPLETE'] = '1'
