3a4808afcda8385686e939bc5309a3f30b249f63
Tristan Cacqueray <tdecacqu@redhat.com>
2016-03-11 12:09:52 -0500

    Update SSL setting to support chain file
    * Move SSL certification generation to the generate_keys function
    * Update sfcreds in place when ssl key are created
    * Reuse the cert as chain file for self-signed
    * Document how to use custom certificate such as letsencrypt
    
    Change-Id: I78da82f3f3de8d304ac4d33bd4bf9b6556ae77e1

--- config/scripts/sfconfig.sh
+++ config/scripts/sfconfig.sh
@@ -175,8 +175,6 @@ function generate_yaml {
     hieraedit.py --yaml ${OUTPUT}/sfcreds.yaml -f ${BUILD}/ssh_keys/gerrit_service_rsa.pub gerrit_service_rsa_pub
     hieraedit.py --yaml ${OUTPUT}/sfcreds.yaml -f ${BUILD}/certs/privkey.pem privkey_pem
     hieraedit.py --yaml ${OUTPUT}/sfcreds.yaml -f ${BUILD}/certs/pubkey.pem  pubkey_pem
-    hieraedit.py --yaml ${OUTPUT}/sfcreds.yaml -f ${BUILD}/certs/gateway.key gateway_key
-    hieraedit.py --yaml ${OUTPUT}/sfcreds.yaml -f ${BUILD}/certs/gateway.crt gateway_crt
 
     ln -sf ${OUTPUT}/sfarch.yaml /etc/puppet/hiera/sf/sfarch.yaml
     ln -sf ${OUTPUT}/sfconfig.yaml /etc/puppet/hiera/sf/sfconfig.yaml
@@ -201,12 +199,11 @@ function generate_keys {
     OUTPUT=${BUILD}/certs
     [ -f ${OUTPUT}/privkey.pem ]        || openssl genrsa -out ${OUTPUT}/privkey.pem 1024
     [ -f ${OUTPUT}/pubkey.pem ]         || openssl rsa -in ${OUTPUT}/privkey.pem -out ${OUTPUT}/pubkey.pem -pubout
-}
 
-function generate_apache_cert {
+    # SSL certificate
     OUTPUT=${BUILD}/certs
     # Generate self-signed Apache certificate
-    cat > openssl.cnf << EOF
+    [ -f openssl.cnf ] || cat > openssl.cnf << EOF
 [req]
 req_extensions = v3_req
 distinguished_name = req_distinguished_name
@@ -221,7 +218,16 @@ subjectAltName=@alt_names
 DNS.1 = ${DOMAIN}
 DNS.2 = auth.${DOMAIN}
 EOF
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FR/O=SoftwareFactory/CN=${DOMAIN}" -keyout ${OUTPUT}/gateway.key -out ${OUTPUT}/gateway.crt -extensions v3_req -config openssl.cnf
+    [ -f ${OUTPUT}/gateway.key ] || {
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FR/O=SoftwareFactory/CN=${DOMAIN}" \
+            -keyout ${OUTPUT}/gateway.key -out ${OUTPUT}/gateway.crt -extensions v3_req -config openssl.cnf
+        hieraedit.py --yaml /etc/puppet/hiera/sf/sfcreds.yaml -f ${OUTPUT}/gateway.key gateway_key
+        hieraedit.py --yaml /etc/puppet/hiera/sf/sfcreds.yaml -f ${OUTPUT}/gateway.crt gateway_crt
+        hieraedit.py --yaml /etc/puppet/hiera/sf/sfcreds.yaml -f ${OUTPUT}/gateway.crt gateway_chain
+    }
+    # Update missing chain configuration (TODO: move this to global config updater (coming soon))
+    grep -q '^gateway_chain:' /etc/puppet/hiera/sf/sfcreds.yaml || \
+        hieraedit.py --yaml /etc/puppet/hiera/sf/sfcreds.yaml -f ${OUTPUT}/gateway.crt gateway_chain
 }
 
 function add_key_known_hosts {
@@ -344,7 +350,6 @@ for i in hiera ssh_keys certs; do
 done
 generate_keys
 if [ ! -f "${BUILD}/generate.done" ]; then
-    generate_apache_cert
     generate_yaml
     touch "${BUILD}/generate.done"
 else
