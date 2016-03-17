96c8ce3406967c202a24acee3bd3d86c582fba9f
George Peristerakis <george.peristerakis@enovance.com>
2016-03-17 10:39:23 -0400

    Encode the project name
    The project name is encoded when making REST calls to create or
    delete a project.
    
    Related-To: #1245
    
    Change-Id: If0f4a0fc1bdec3f11f2e83e7de17ef7072dd37ca
    Depends-On: 5c3de21cba221d7a090b380835b4d17d57de5f9e

--- puppet/modules/gateway/files/dashboard.js
+++ puppet/modules/gateway/files/dashboard.js
@@ -131,8 +131,9 @@ function mainController($scope, $http) {
     $scope.createProject = function() {
         $scope.errors = false;
         $scope.loading = true;
+        var name = '===' + btoa($scope.data.scope);
 
-        $http.put('/manage/project/' + $scope.data.name , $scope.data)
+        $http.put('/manage/project/' + name + '/' , $scope.data)
             .success(function(data) {
                 $scope.data = {};
                 initProjects();
@@ -148,7 +149,9 @@ function mainController($scope, $http) {
         if (window.confirm('Delete project ' + name + '?') ) {
             $scope.errors = false;
             $scope.loading = true;
-            $http.delete('/manage/project/' + name)
+            name = '===' + btoa(name);
+
+            $http.delete('/manage/project/' + name + '/')
                 .success(function(data) {
                     $scope.data = {};
                     initProjects();
