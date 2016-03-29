56bc161075d5892c05d87d8060473073592cbbe6
Fabien Boucher <fboucher@redhat.com>
2016-03-29 13:06:11 +0000

    WIP playbook to rename project on SF
    Change-Id: I9fec75a692f6559725c881bbc80cc8c090b4f6cb

---
+++ tools/rename_project/rename-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+[ -z "$1" ] && {
+    echo "Please provide old name as arg 1"
+    exit 1
+}
+[ -z "$2" ] && {
+    echo "Please provide new name as arg 2"
+    exit 1
+}
+
+echo "update account_project_watches set project_name = \"$2\" where project_name = \"$1\";" | mysql gerrit
+echo "update changes set dest_project_name = \"$2\", created_on = created_on where dest_project_name = \"$1\";" | mysql gerrit
+mkdir -p "/home/gerrit/site_path/git/$(dirname $2)"
+[ -d "/home/gerrit/site_path/git/${1}.git" ] && mv "/home/gerrit/site_path/git/${1}.git" "/home/gerrit/site_path/git/${2}.git" || true
