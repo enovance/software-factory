a974166f9a3d4680231a1ae9a093ed0ff7fc3277
Fabien Boucher <fboucher@redhat.com>
2016-04-18 13:25:18 +0000

    Manage Gerrit replication.config in the configuration repo
    - Add upgrade mecanism to propose the actual replication.config
      file as a patch to the config repo.
    - By default provide a config repo with the default gerrit/replication.config
    - Remove the default replication if the config repo to managesf
    - Remove special sudoers right for gerrit
    - Add a check of replication.config in the config-check job
    - Add a simple copy of replication.config to the gerrit host
      when config-update is executed.
    - Adapt replication functional test
    - Fix test_zuul test to be make teardown waiting for the config-update
      to finished (fixed a race condition conflict with that change)
    - Push the backup file in the artifact directory
    
    Change-Id: I1d758681d66fd9e9a0f50bf01254706b44afd2e8

--- tools/get_last_autogen_upgrade_config_review.py
+++ tools/get_last_autogen_upgrade_config_review.py
@@ -7,7 +7,7 @@ import requests
 import json
 import sys
 
-search_string = "Upgrade of base JJB%2FZuul%2FNodepool files"
+search_string = "Upgrade of base config repository files"
 
 r = requests.get(
     '%s/r/changes/?q=%s' % (sys.argv[1], search_string))
