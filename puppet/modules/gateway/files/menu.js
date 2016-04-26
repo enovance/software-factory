6bd37b41fbb6f6e5d5a27e6a9b96c57435da5603
George Peristerakis <george.peristerakis@enovance.com>
2016-04-26 14:38:26 -0500

    [WIP] Experimental top menu
    Change-Id: Ie95e42b489bbec70efa02759425583a7cf6aeebb

--- puppet/modules/gateway/files/menu.js
+++ puppet/modules/gateway/files/menu.js
@@ -56,10 +56,33 @@ function initAuth() {
     displaySignIn();
 };
 
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
+};
+
+function initTopMenu() {
+  document.getElementById("navbar-workflow").style.display = "none";
+  document.getElementById("navbar-collaboration").style.display = "none";
+  document.getElementById("navbar-workflow-btn").onclick = function(event) {
+    toggle_visibility("navbar-workflow");
+  };
+  document.getElementById("navbar-collaboration-btn").onclick = function(event) {
+    toggle_visibility("navbar-collaboration");
+  };
+};
+ 
 /** Init function 
  */
 if (document.body) {
-    initAuth();
+  initAuth();
+  initTopMenu();
 } else {
-    document.onload = function() {initAuth();};
+  document.onload = function() {
+    initAuth();
+    initTopMenu();
+  };
 }
