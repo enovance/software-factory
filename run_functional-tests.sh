43c544bb1fae900011a0e6aa007145315545c492
Christian Schwede <cschwede@redhat.com>
2016-06-15 20:35:39 -0400

    WIP: Run functional tests after FQDN change
    Change-Id: I4dcd352d8054322187ae968387a646a00e8156ea

--- run_functional-tests.sh
+++ run_functional-tests.sh
@@ -69,7 +69,6 @@ case "${TEST_TYPE}" in
         run_bootstraps
         run_serverspec_tests
         run_health_base
-        run_functional_tests
         run_provisioner
         run_backup_start
         lxc_stop
@@ -82,6 +81,7 @@ case "${TEST_TYPE}" in
         run_sfconfig
         run_serverspec_tests
         fetch_bootstraps_data
+        run_functional_tests
         ;;
     "upgrade")
         ./fetch_image.sh ${SF_PREVIOUS_VER} || fail "Could not fetch ${SF_PREVIOUS_VER}"
