abe29150ad772f4ec338deef141c41e5262108cd
mhuin <mhuin@redhat.com>
2016-07-04 12:42:52 +0200

    Add story about adding a user to a project with the GUI
    Change-Id: I2646a7673ab4f076db1e9ddd0b6edb0e2f55af9f

--- tests/gui/user_stories/test_stories.py
+++ tests/gui/user_stories/test_stories.py
@@ -29,6 +29,10 @@ except ImportError:
 from tests.gui.base import BaseGuiTest, caption, snapshot_if_failure
 from tests.gui.base import loading_please_wait
 from tests.functional import config
+from tests.functional import utils
+
+
+test_project = "Demo_Project"
 
 
 ENV = os.environ
@@ -123,8 +127,13 @@ class ShellRecorder(BaseGuiTest):
 
 class TestAdministratorTasks(ShellRecorder):
 
+    def tearDown(self):
+        sfmanager = utils.ManageSfUtils(config.GATEWAY_URL)
+        sfmanager.deleteProject(test_project, config.ADMIN_USER)
+
     @snapshot_if_failure
     def test_create_project(self):
+        """Create a project in the GUI"""
         self.driver.get(config.GATEWAY_URL)
         self.login_as(config.ADMIN_USER, config.ADMIN_PASSWORD)
 
@@ -142,7 +151,7 @@ class TestAdministratorTasks(ShellRecorder):
         msg = ("Define your project here. "
                "Eventually specify an upstream repo to clone from.")
         with caption(self.driver, msg):
-            self.highlight("#projectname").send_keys("Demo_Project")
+            self.highlight("#projectname").send_keys(test_project)
             self.highlight("#description").send_keys("Test Description")
             ele = self.highlight_button("Create project")
             ele.click()
@@ -150,7 +159,55 @@ class TestAdministratorTasks(ShellRecorder):
         msg = "Now your project is ready."
         with loading_please_wait(self.driver) as driver:
             with caption(driver, msg):
-                self.highlight_link("Demo_Project").click()
+                self.highlight_link(test_project).click()
+        msg = "Thank you for watching !"
+        with caption(self.driver, msg):
+            self.highlight("body")
+
+    @snapshot_if_failure
+    def test_add_user_to_project(self):
+        """Add a user to a project in the GUI"""
+        sfmanager = utils.ManageSfUtils(config.GATEWAY_URL)
+        sfmanager.createProject(test_project, config.ADMIN_USER)
+        # must log in as user2 once before
+        sfmanager.list_active_members(config.USER_2)
+
+        self.driver.get(config.GATEWAY_URL)
+        self.login_as(config.ADMIN_USER, config.ADMIN_PASSWORD)
+
+        msg = ("Log in as administrator, "
+               "then go to the dashboard from the top menu.")
+        with caption(self.driver, msg) as driver:
+            driver.get("%s/dashboard/" % config.GATEWAY_URL)
+
+        msg = ("Click on the membership management button "
+               "next to the project on which your user will work.")
+        with caption(self.driver, msg):
+            mgmt_btn_xpath = '//table/tbody/tr[td="%s"]/td[4]/button[1]'
+            self.highlight_by_xpath(mgmt_btn_xpath % test_project).click()
+
+        msg = ("Look for the user to add in the search box.")
+        with caption(self.driver, msg):
+            user_search_xpath = (".//*[@id='modal_create_members']/div/div/"
+                                 "form/div[1]/input")
+            searchbox = self.highlight_by_xpath(user_search_xpath)
+            searchbox.send_keys(config.USER_2)
+
+        msg = ("The user appears in the list below the search box.")
+        with caption(self.driver, msg):
+            user_found = (".//*[@id='modal_create_members']/div/div/form/"
+                          "div[1]/table/tbody/tr[contains(.,'%s')]")
+            self.highlight_by_xpath(user_found % config.USER_2)
+
+        msg = ("Let's add the user as a Core Developer...")
+        with caption(self.driver, msg):
+            core_btn = (user_found + "/td[3]/input")
+            self.highlight_by_xpath(core_btn % config.USER_2).click()
+
+        msg = ("...And submit it.")
+        with caption(self.driver, msg):
+            ele = self.highlight_button("Submit")
+            ele.click()
 
         msg = "Thank you for watching !"
         with caption(self.driver, msg):
