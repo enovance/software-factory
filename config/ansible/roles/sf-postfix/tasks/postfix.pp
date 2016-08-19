29baf34d027fade338a7c685bb1a3da8f6e771f5
Nicolas Hicher <nhicher@redhat.com>
2016-08-19 15:38:07 -0400

    Ansible: create sf-postfix
    [WIP] do not review
    
    * migrate erb templates to jinja
    * add file from puppet module (to manage monit)
    * ...
    
    Change-Id: I58b8cbbe0b9f8c4b2010ed0a5c61ef2cb7b6b78e

---
+++ config/ansible/roles/sf-postfix/tasks/postfix.pp
@@ -0,0 +1,71 @@
+class postfix {
+  include ::monit
+
+  $network = hiera('network')
+  $auth = hiera('authentication')
+  $smtp_relay = $network['smtp_relay']
+  $domain = hiera('fqdn')
+
+  group { 'postfix':
+    ensure => present,
+  }
+
+  group { 'postdrop':
+    ensure => present,
+  }
+
+  user { 'postfix':
+    ensure  => present,
+    gid     => 'postfix',
+    require => Group['postfix'],
+  }
+
+  package { 'postfix':
+    ensure  => installed,
+    require => [User['postfix'], Group['postfix']],
+  }
+
+  exec { '/etc/mailname':
+    command => 'hostname --fqdn > /etc/mailname',
+    unless  => '/usr/bin/grep `hostname --fqdn` /etc/mailname',
+    path    => '/usr/sbin/:/usr/bin/:/bin/',
+  }
+
+  file { '/etc/postfix':
+    ensure  => directory,
+    require => Package['postfix'],
+  }
+
+  file { '/etc/postfix/main.cf':
+    ensure  => file,
+    content => template('postfix/main.cf.erb'),
+    require => [Package['postfix'], File['/etc/postfix']],
+    replace => true,
+  }
+
+  file { '/etc/postfix/virtual':
+    ensure  => file,
+    content => template('postfix/virtual.erb'),
+    require => [Package['postfix'], File['/etc/postfix']],
+    replace => true,
+  }
+
+  service { 'postfix':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require    => [Package['postfix'], Exec['/etc/mailname']],
+    subscribe  => File['/etc/postfix/main.cf'],
+  }
+
+  file { '/etc/monit.d/postfix':
+    ensure  => file,
+    source  => 'puppet:///modules/postfix/monit',
+    notify  => Service['monit'],
+  }
+
+  exec {'postfix_update_virtual':
+    command => '/usr/sbin/postmap /etc/postfix/virtual',
+    require => File['/etc/postfix/virtual'],
+  }
+}
