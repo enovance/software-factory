1d0d20ee43c4323ff290ccf3ac5f7697e4b5afec
mhuin <mhuin@redhat.com>
2016-09-13 12:56:29 +0200

    Display API key, gerrit htpasswd in settings page
    Change-Id: I5464a8674248d73256a67205bab1be57a588948b
    Related-To: #1537

--- config/ansible/roles/sf-gateway/files/user_settings.js
+++ config/ansible/roles/sf-gateway/files/user_settings.js
@@ -28,21 +28,50 @@ angular.module('sfSettings', []).controller('mainController', function($scope, $
     $scope.showErrorAlert = false;
     $scope.successTextAlert = "";
 
-    // Get user informations
+// Get user informations
     $http.get('/manage/services_users/?username='+localStorage.getItem("sf.username")).
         success(function(data) {
             $scope.user = data;
             // Remove un-managed data
-            delete $scope.user.cauth_id;
             delete $scope.user.id;
 
-            // Keep a copy of the original data
+            if ($scope.user.idp_sync == "True") {
+                $scope.user.idp_sync = true
+            } else {
+                $scope.user.idp_sync = false
+            }
             $scope.original = angular.copy($scope.user)
         }).
         error(function(data) {
             $scope.showErrorAlert = true;
         });
 
+    // Get htpasswd status
+    $http.get('/manage/htpasswd/')
+        .success( function (data) {
+          $scope.htpasswd_set = true;
+          $scope.user.gerrit_api_key = "SECRET";
+        })
+        .error( function (data) {
+          $scope.htpasswd_set = false;
+          $scope.user.gerrit_api_key = "-";
+        })
+        .catch( function (data) {
+          $scope.htpasswd_set = false;
+          $scope.user.gerrit_api_key = "-";
+        });
+
+    // Get API token
+    $http.get('/auth/apikey/?cauth_id='+localStorage.getItem("ls.id_token")).
+        success(function(data) {
+            $scope.user.api_key = data.api_key
+        }).
+        error(function() {
+            $http.post('/auth/apikey/?cauth_id='+localStorage.getItem("ls.id_token")).
+                then(function(data) {
+                    $scope.user.api_key = data.api_key
+                })
+        });
     // Form submit action
     $scope.save = function() {
         if ($scope.original.idp_sync != $scope.user.idp_sync) {
@@ -58,14 +87,47 @@ angular.module('sfSettings', []).controller('mainController', function($scope, $
         $scope.user.full_name = $scope.user.fullname;
         $http.put('/manage/services_users/?username='+localStorage.getItem("sf.username"), $scope.user).
             success(function() {
-                // Update original data only on success
                 $scope.original = angular.copy($scope.user)
                 $scope.showSuccessAlert = true;
             }).
             error(function() {
-                // If update fail, restore original data
                 $scope.user = angular.copy($scope.original);
                 $scope.showErrorAlert = true;
             });
     };
+    $scope.regen_api_key = function() {
+            $http.delete('/auth/apikey/?cauth_id='+localStorage.getItem("ls.id_token")).
+                then(function() {
+                    $http.post('/auth/apikey/?cauth_id='+localStorage.getItem("ls.id_token")).
+                        success(function(data) {
+                            $scope.user.api_key = data.api_key
+                        })
+                });
+    };
+
+    $scope.htpasswd_disable = function(name) {
+        $scope.errors = false;
+        $http.delete('/manage/htpasswd/')
+            .success(function(data) {
+                $scope.htpasswd_set = false;
+                $scope.gerrit_api_key = "";
+            })
+            .error(function(data) {
+                $scope.errors = data;
+            });
+    };
+
+    $scope.htpasswd_enable = function(name) {
+        $scope.errors = false;
+        $http.put('/manage/htpasswd/')
+            .success(function(data) {
+                $scope.user.gerrit_api_key = data;
+                $scope.htpasswd_set = true;
+            })
+            .error(function(data) {
+                $scope.errors = data;
+                $scope.htpasswd_set = false;
+            });
+    };
+
 });
