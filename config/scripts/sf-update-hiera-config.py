c6a4b8c0143b6e15cb3bc7f69095d12792f26651
Tristan Cacqueray <tdecacqu@redhat.com>
2016-11-16 02:20:08 +0000

    sfconfig: enable setting static hostnames
    This change enables adding custom hosts definition to /etc/hosts.
    This is particularly useful when trying to connect 2 SF deployment
    that do not have registered DNS.
    
    Change-Id: Ic310a7c7281ab0538b7a301575ddd4157cb22db7

--- config/scripts/sf-update-hiera-config.py
+++ config/scripts/sf-update-hiera-config.py
@@ -148,6 +148,11 @@ def update_sfconfig(data):
                 value[name.replace('-', '_')] = value.pop(name)
                 dirty = True
 
+    # 2.3.0: enable static hosts settings
+    if 'static_hostnames' not in data['network']:
+        data['network']['static_hostnames'] = []
+        dirty = True
+
     return dirty
 
 
