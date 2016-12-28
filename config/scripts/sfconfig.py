22f35f63b0fa2c9077adb4eba81088518e6e1184
Tristan Cacqueray <tdecacqu@redhat.com>
2016-12-28 08:34:54 +0000

    gerrit mysql: make the roles generic
    Story: 40
    Change-Id: I379efd3c85278c3140083ba2a6a1c3a5b49a0596

--- config/scripts/sfconfig.py
+++ config/scripts/sfconfig.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import yaml
+import uuid
 
 
 def append_legacy(allvars_file, args):
@@ -26,17 +27,41 @@ def yaml_load(filename):
 def generate_role_vars(allvars_file, args):
     """ This function 'glue' all roles and convert sfconfig.yaml """
     secrets = yaml_load("%s/secrets.yaml" % args.lib)
+    mysql_databases = secrets.setdefault('mysql_databases', {})
+
+    arch = yaml_load(args.arch)
     sfconfig = yaml_load(args.sfconfig)
 
+    # Check for rolevars secrets and mysql databases
+    for role in arch["roles"]:
+        role_vars = yaml_load("%s/roles/sf-%s/defaults/main.yml" % (
+                              args.ansible_root, role))
+        for key, value in role_vars.items():
+            if str(value).strip().replace('"', '') == 'CHANGE_ME':
+                if key not in secrets:
+                    secrets[key] = str(uuid.uuid4())
+        if '%s_mysql_db' % role in role_vars:
+            dbname = role_vars['%s_mysql_db' % role]
+            mysql_databases[dbname] = {
+                'hosts': ['localhost', arch["roles"][role][0]["hostname"]],
+                'user': role_vars['%s_mysql_user' % role],
+                'password': secrets['%s_mysql_password' % role],
+            }
+
     yaml.dump(secrets, open("%s/secrets.yaml" % args.lib, "w"))
-    if secrets:
-        yaml.dump(secrets, allvars_file)
+    yaml.dump(secrets, allvars_file)
 
     # Set absolute urls and hostnames
     glue = {}
 
     # Fix url used in services
     glue["gateway_url"] = "https://%s" % sfconfig["fqdn"]
+    glue["mysql_host"] = arch["roles"]["mysql"][0]["hostname"]
+
+    if "gerrit" in arch["roles"]:
+        glue["gerrit_pub_url"] = "%s/r/" % glue["gateway_url"]
+        glue["gerrit_email"] = "gerrit@%s" % sfconfig["fqdn"]
+        glue["gerrit_mysql_host"] = glue["mysql_host"]
 
     yaml.dump(glue, allvars_file, default_flow_style=False)
 
